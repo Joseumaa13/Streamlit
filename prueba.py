b4b9ca2f04c8d645e379101d1a03d8d65dd62e99
Joseumaa13 <joseand64@gmail.com>
2022-11-21 11:43:02 -0600

    Textos

---
+++ prueba.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Hola Mundo")
+st.header("Primer Encabezado")
+st.subheader("Primer subEncabezado")
+st.write("Texto normal")
+
+st.header("Segundo Encabezado")
+st.subheader("Segundo Encabezado")
+st.write("Texto bajo el segundo encabezado")
+st.markdown("*Itálica*, **Negrita**, [UCR](https://www.ucr.ac.cr/)")
